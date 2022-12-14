fd79969e30fc5d76e001bcc553927836aa9da568
techstack-io <81205746+techstack-io@users.noreply.github.com>
2022-12-13 23:38:33 -0600

    added dynamic content

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'mK+Vq3n0SUnsWqzsUeIuQA=='),
   },
 });
